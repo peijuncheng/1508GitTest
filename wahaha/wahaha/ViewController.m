e47c47284fdef9c6156aedc8e752a8c51c0e87f4
tarena <tarena@tarenadeiMac-7.local>
2015-10-08 20:06:48 +0800

    first log

---
+++ wahaha/wahaha/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  wahaha
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"11111");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
